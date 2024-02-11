7b54c8d98f0bfc08c81964a2438d7a32d55d42ef
David <david.villada@correo.tdea.edu.co>
2024-02-11 14:26:24 -0500

    Modificamos las funcionalidades de encriptar y desencriptar además de estilar mejor el archivo... Proximamente responsive

--- index.js
+++ index.js
@@ -3,63 +3,79 @@ let botonEncriptar = document.getElementById("botonEncriptar");
 let resultadoEncriptado = document.getElementById("txt-campo-resultado");
 
 let cambios = {
-    "a" : "ai",
-    "e" : "enter",
-    "i" : "imes",
-    "o" : "ober",
-    "u" : "ufat"
+    "a": "ai",
+    "e": "enter",
+    "i": "imes",
+    "o": "ober",
+    "u": "ufat"
 }
 
 let palabrasDesencriptar = {
-    "ai" : "a",
-    "enter" : "e",
-    "imes" : "i",
-    "ober" : "o",
-    "ufat" : "u"
+    "ai": "a",
+    "enter": "e",
+    "imes": "i",
+    "ober": "o",
+    "ufat": "u"
 }
 
 function encriptar() {
     let texto = entradaEncriptar.value;
-    console.log(texto);
-
-    let textoEncriptado = "";
-
-    for (let i = 0; i < texto.length; i++) {
-        let caracter = texto[i];
-        if(caracter in cambios){
-            textoEncriptado += cambios[caracter];
-            console.log(cambios[caracter]);
+    // Validamos si el texto ingresado es vacio
+    if (texto === "") {
+        alert("Por favor, ingrese un texto para encriptar");
+        return;
+    } else {
+        // validamos que el texto ingresado no contenga mayusculas o caracteres especiales
+        if (texto.match(/[A-Z]/) || texto.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/)) {
+            alert("Por favor, ingrese un texto en minusculas y sin caracteres especiales");
+            return;
         } else {
-            textoEncriptado += caracter;
+            let textoEncriptado = "";
+            for (let i = 0; i < texto.length; i++) {
+                let letra = texto[i];
+                if (cambios[letra]) {
+                    textoEncriptado += cambios[letra];
+                } else {
+                    textoEncriptado += letra;
+                }
+            }
+            resultadoEncriptado.innerHTML = textoEncriptado;
+            mostrarResultado();
+            console.log(textoEncriptado);
+            limparCajasDeTexto();
         }
     }
-
-
-    resultadoEncriptado.innerHTML = textoEncriptado;
-
-    mostrarResultado();
-    console.log(textoEncriptado)
-    limparCajasDeTexto();
 }
 
 function desencriptar() {
+    // Validamos si el texto ingresado es vacio
     let texto = entradaEncriptar.value;
-    let textoDesencriptado = "";
+    if (texto === "") {
+        alert("Por favor, ingrese un texto para desencriptar");
+        return;
+    } else {
+        // validamos que el texto ingresado no contenga mayusculas o caracteres especiales
+        if (texto.match(/[A-Z]/) || texto.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/)) {
+            alert("Por favor, ingrese un texto en minusculas y sin caracteres especiales");
+            return;
+        } else {
+            let texto = entradaEncriptar.value;
+            let textoDesencriptado = "";
 
-    textoDesencriptado = texto.replace(/ai/g, "a");
-    textoDesencriptado = textoDesencriptado.replace(/enter/g, "e");
-    textoDesencriptado = textoDesencriptado.replace(/imes/g, "i");
-    textoDesencriptado = textoDesencriptado.replace(/ober/g, "o");
-    textoDesencriptado = textoDesencriptado.replace(/ufat/g, "u");
+            textoDesencriptado = texto.replace(/ai/g, "a");
+            textoDesencriptado = textoDesencriptado.replace(/enter/g, "e");
+            textoDesencriptado = textoDesencriptado.replace(/imes/g, "i");
+            textoDesencriptado = textoDesencriptado.replace(/ober/g, "o");
+            textoDesencriptado = textoDesencriptado.replace(/ufat/g, "u");
 
-    resultadoEncriptado.innerHTML = textoDesencriptado;
+            resultadoEncriptado.innerHTML = textoDesencriptado;
 
-    mostrarResultado();
-    console.log(textoDesencriptado)
-    limparCajasDeTexto();
+            mostrarResultado();
+            console.log(textoDesencriptado)
+            limparCajasDeTexto();
+        }
+    }
 }
-
-
 
 function limparCajasDeTexto() {
     entradaEncriptar.value = "";
@@ -82,14 +98,32 @@ function mostrarResultado() {
     botonCopiar.style.display = "block";
 }
 
+function mostrarImagen() {
+    let imagen = document.getElementById("imagen-resultado");
+    imagen.style.display = "block";
+
+    let textoInformacion = document.getElementById("txt-campo-info");
+    textoInformacion.style.display = "block";
+
+    let textoInstrucciones = document.getElementById("txt-campo-instrucciones");
+    textoInstrucciones.style.display = "block";
+
+    let campoResultado = document.getElementById("txt-campo-resultado");
+    campoResultado.style.display = "none";
+
+    let botonCopiar = document.getElementById("boton-copiar");
+    botonCopiar.style.display = "none";
+
+}
+
 function copiarTexto() {
-    navigator.permissions.query({name: 'clipboard-write'}).then(
+    navigator.permissions.query({ name: 'clipboard-write' }).then(
         result => {
             if (result.state == 'granted' || result.state == 'prompt') {
                 navigator.clipboard.writeText(resultadoEncriptado.innerHTML).then(
                     () => {
                         console.log("Texto copiado");
-
+                        copiadoExitoso();
                     }
                 )
             } else {
@@ -102,6 +136,16 @@ function copiarTexto() {
 function copiadoExitoso() {
     let botonCopiar = document.getElementById("boton-copiar");
     botonCopiar.innerHTML = "Copiado";
-    botonCopiar.style.backgroundColor = "#00ff00";
-    botonCopiar.style.borderColor = "#00ff00";
+    botonCopiar.style.backgroundColor = "green";
+    botonCopiar.style.color = "#ffffff";
+    botonCopiar.style.cursor = "not-allowed";
+    botonCopiar.disabled = true;
+
+    setTimeout(() => {
+        botonCopiar.innerHTML = "Copiar";
+        botonCopiar.style.backgroundColor = "#0A3871";
+        botonCopiar.style.color = "#FFFFFF";
+        botonCopiar.style.cursor = "pointer";
+        botonCopiar.disabled = false;
+    }, 1000);
 }
